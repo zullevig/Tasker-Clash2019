045ebe180b6d8e76fd8799454ce43c41944c0a31
Zachary Ullevig <zullevig@bignerdranch.com>
2019-09-27 09:09:29 -0400

    first commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,7 @@ import FluentMySQL
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    
     // Register providers first
     try services.register(FluentMySQLProvider())
     try services.register(LeafProvider())
@@ -39,5 +40,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: TestModel.self, database: .mysql)
+    migrations.add(model: Object.self, database: .mysql)
+    migrations.add(model: CueItem.self, database: .mysql)
+    migrations.add(model: WorkItem.self, database: .mysql)
+    migrations.add(model: Job.self, database: .mysql)
+    migrations.add(model: WorkItemEvent.self, database: .mysql)
+    migrations.add(model: JobEvent.self, database: .mysql)
     services.register(migrations)
+    
+//    let jsonData = try JSONEncoder().encode(WorkItemDuration.hardMaximumTime(30))
+//    let json = String(data: jsonData, encoding: .utf8)
+//    print(json)
 }
